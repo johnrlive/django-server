590f7e854aab61fe07d5af22b3e420230c9e2528
John R Live <john@addlocal.com>
2016-07-06 13:49:09 -0400

    set variables to app_name and domain

--- nginx.sh
+++ nginx.sh
@@ -1,11 +1,14 @@
+set $APP_NAME firstsite;
+set $DOMAIN www.firstsite.com;
+
 server {
     listen 80;
-    server_name firstsite.com www.firstsite.com;
+    server_name $DOMAIN;
 
     location = /favicon.ico { access_log off; log_not_found on; }
-   
-    error_log /var/log/firstsite.com_error.log;
-    access_log /var/log/firstsite.com_access.log;
+
+    error_log /var/log/$APP_NAME.com_error.log;
+    access_log /var/log/$APP_NAME.com_access.log;
 
     # Maximum file upload size.
     client_max_body_size 64M;
@@ -18,28 +21,28 @@ server {
     gzip_http_version 1.0;
     gzip_proxied any;
     gzip_vary on;
- 
+
     location /static/ {
         access_log off;
         expires 3600;
-        alias /var/www/firstsite/static/;
+        alias /var/www/$APP_NAME/static/;
     }
 
     # Set a longer expiry for CACHE/, because the filenames are unique.
     location /static/CACHE/ {
         access_log off;
         expires 864000;
-        alias /var/www/firstsite/static/CACHE/;
+        alias /var/www/$APP_NAME/static/CACHE/;
     }
 
     # Only server /media/images by default, not e.g. original_images/.
     location /media/images/ {
         expires 864000;
-        alias /var/www/firstsite/media/images/;
+        alias /var/www/$APP_NAME/media/images/;
     }
 
     location / {
         include         uwsgi_params;
-        uwsgi_pass      unix:/run/uwsgi/firstsite.sock;
+        uwsgi_pass      unix:/run/uwsgi/$APP_NAME.sock;
     }
 }
